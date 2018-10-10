2ec455de44c771deed3cf36ea02fb152e3971af9
konstantin <arkov.constantin@yandex.ru>
2018-10-11 01:32:14 +0300

    add logging

--- task1/filling_db.py
+++ task1/filling_db.py
@@ -2,17 +2,18 @@
 # -*- coding: utf-8 -*-
 import csv, sqlite3
 from timer import Timer
+import logging
 
 
 def cached(func):
     cache = {}
 
-    def new_func(x, cur, con, i, log):
+    def new_func(x, cur, con, i):
         table = {'Original Crime Type Name': 'crime_type', 'Disposition': 'disposition', 'Address Type': 'address_type'}
         if i[x] not in cache:
             cache[i[x]] = len(cache) + 1
-            log.write(f'[+] new record in table "{table[x]}"\n')
-            func(x, cur, con, i, log)
+            logging.debug(f'Create new record in table "{table[x]}"')
+            func(x, cur, con, i)
 
         return cache[i[x]]
 
@@ -20,24 +21,24 @@ def cached(func):
 
 
 @cached
-def insert_crime_type(col_name, cur, con, i, log):
+def insert_crime_type(col_name, cur, con, i):
     cur.execute(f'insert into crime_type (type) values ("{i[col_name]}");')
     con.commit()
 
 
 @cached
-def insert_disposition(col_name, cur, con, i, log):
+def insert_disposition(col_name, cur, con, i):
     cur.execute(f'insert into disposition (code) values ("{i[col_name]}");')
     con.commit()
 
 
 @cached
-def insert_address_type(col_name, cur, con, i, log):
+def insert_address_type(col_name, cur, con, i):
     cur.execute(f'insert into address_type (type) values ("{i[col_name]}");')
     con.commit()
 
 
-def insert_crime(log, i, cur, con, id_crime_type, id_disposition, id_address_type):
+def insert_crime(i, cur, con, id_crime_type, id_disposition, id_address_type):
     cur.execute(f'INSERT INTO Crime (Id_Crime_Type, Report_Date, Call_Date, Offense_Date, Call_Time, Call_Date_Time,'
                 f' Id_Disposition, Address, City, State, Agency_Id, Id_Address_Type, Common_Location)'
                 f' VALUES ({id_crime_type},"{i["Report Date"]}","{i["Call Date"]}","{i["Offense Date"]}",'
@@ -46,26 +47,26 @@ def insert_crime(log, i, cur, con, id_crime_type, id_disposition, id_address_typ
                 f'"{i["Common Location"]}")')
 
     con.commit()
-    log.write('[+] new record in table "Crime"\n')
+    logging.debug(f'Create new record in table "Crime"')
 
 
 def filling_db(db_name):
+    logging.basicConfig(format=u'[%(asctime)s] %(levelname)-8s %(message)s', level=logging.DEBUG, filename=u'mylog.log')
     con = sqlite3.connect(db_name)
     cur = con.cursor()
 
-    with open("log.txt", "a") as log:
-        with Timer(cur, log):
-            with open('police-department-calls-for-service.csv', 'r') as fin:
-                dr = csv.DictReader(fin)
-                for i in dr:
-                    id_crime_type = insert_crime_type('Original Crime Type Name', cur, con, i, log)
-                    id_disposition = insert_disposition('Disposition', cur, con, i, log)
-                    id_address_type = insert_address_type('Address Type', cur, con, i, log)
+    with Timer(cur):
+        with open('police-department-calls-for-service.csv', 'r') as fin:
+            dr = csv.DictReader(fin)
+            for i in dr:
+                id_crime_type = insert_crime_type('Original Crime Type Name', cur, con, i)
+                id_disposition = insert_disposition('Disposition', cur, con, i)
+                id_address_type = insert_address_type('Address Type', cur, con, i)
 
-                    insert_crime(log, i, cur, con, id_crime_type, id_disposition, id_address_type)
-                    # pass
+                insert_crime(i, cur, con, id_crime_type, id_disposition, id_address_type)
 
-                con.close()
+            con.close()
+
 
 '''
 Такая проверка нужна для того, чтобы скрипт не выполнялся при импорте.
--- task1/timer.py
+++ task1/timer.py
@@ -1,19 +1,18 @@
 import time
 from datetime import datetime
 from functools import reduce
+import logging
 
 
 class Timer:
-    def __init__(self, cur, log):
+    def __init__(self, cur):
         self.cur = cur
-        self.log = log
         self.begin = []
         self.end = []
         self.tables = ['Crime', 'crime_type', 'address_type', 'disposition']
 
     def __enter__(self):
-        # print('started')
-        self.log.write('started\n')
+        logging.info('started')
 
         for table in self.tables:
             self.cur.execute(f"select max(id) from {table}")
@@ -37,7 +36,5 @@ class Timer:
 
         count = reduce(lambda x, y: x + y, [v - self.begin[i] for i, v in enumerate(self.end)])
 
-        # print('finished in', ((datetime.now() - self.start).seconds) / 60, 'min')
-        self.log.write(f'finished in {((datetime.now() - self.start).seconds) / 60} min\n')
-        self.log.write(f'num of recorded row {count}\n')
-        # print('num of recorded row ',count)
+        logging.info(f'finished in {((datetime.now() - self.start).seconds) / 60} min')
+        logging.info(f'num of recorded row {count}')
